4181a17570234d906a25aa43670cefdc094214a8
Andrey Drozdov <drozdov_andrey@list.ru>
2022-06-21 21:15:16 +0300

    examples 005

---
+++ Examples005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введте имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, этио же МАША!");
+}
+else
+{
+    Console.WriteLine("Привет, ");
+    Console.WriteLine(username);
+}
